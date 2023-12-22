b6117895751bbf408f735f77672ff192067f3701
jpofficials7 <jpofficials7@gmail.com>
2023-12-22 20:20:52 +0530

    setting up the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="./public/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
   );
-}
-
+};
 export default App;
